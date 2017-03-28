f0a9435d75c5b5b7898cda104098bbe7e0978e2d
MilesZhao <mileszhao1990@yahoo.com>
2017-03-28 14:46:43 -0400

    Build login page

---
+++ login.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+  <h1> This is a login page</h1>
+  <form method = "post" action="index.php">
+  <strong>Username:</strong><input type="test" name="reg_uname" value=""/><br><br>
+  <strong>Password:</strong><input type="password" name="reg_password" value=""/><br><br>
+  <input type ="hidden" name="action" value="test_user"><br>
+  <input type="submit" value="Login"/>
+  </form>
+</body>
+</html>
