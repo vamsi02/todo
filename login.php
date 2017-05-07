f136d3b346b14be80dfc859a14532ca76bd325dc
vamsi krishna surapaneni <vks35@afsconnect2.njit.edu>
2017-05-07 16:06:11 -0400

    styleing login

--- login.php
+++ login.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ </head>
+<body>
+<div class="container">&nbsp;</div>
+<div class="container">
+<div class="row">
 <html>
 <body>
   <h1> This is a login page</h1>
