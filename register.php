72e4ea1e494b68815187cd634eb17b7777ce8757
vamsi krishna surapaneni <vks35@afsconnect2.njit.edu>
2017-05-07 17:34:49 -0400

    register fourm

--- register.php
+++ register.php
@@ -1,18 +1,57 @@
 <html>
 <body>
-	<h1> This is a register page </h1>
-	<form method = 'post' action = 'index.php'>
-		<strong> Username : </strong> <input type = 'text' name = 'reg_username' value =
-		"" /> < /br> < /br>
-
-		<strong> Password : </strong> <input type = "password" name = "reg_password"
-		value="" /> < /br> < /br>
-		<input type = "hidden" name = "action" value = "registar">
-	</form>
-
-	<form action = "login.php" method = 'post'>
-		<input type = 'submit' value = 'try to login' />
-	</form>
+	<<div class="container">&nbsp;</div>
+<div class="container">
+<div class="row">
+<form method="post" action="index.php" name="form1">
+	<div class="form-group row">
+		<label for="example-text-input" class="col-md-3 col-form-label">First Name</label>
+		<div class="col-md-8">
+			<input class="form-control" type="text" name="user_fname" placeholder="Enter First Name" id="example-text-input">
+		</div>
+	</div>
+	<div class="form-group row">
+		<label for="example-text-input" class="col-md-3 col-form-label">Last Name</label>
+		<div class="col-md-8">
+			<input class="form-control" type="text" name="user_lname" placeholder="Enter Last Name" id="example-text-input">
+		</div>
+	</div>
+	<div class="form-group row">
+		<label for="example-text-input" class="col-md-3 col-form-label">Phone</label>
+		<div class="col-md-8">
+			<input class="form-control" type="text" name="user_pnumber" placeholder="Enter Phone" id="example-text-input">
+		</div>
+	</div>
+	<div class="form-group row">
+		<label for="example-text-input" class="col-md-3 col-form-label">Email</label>
+		<div class="col-md-8">
+			<input class="form-control" type="email" name="reg_username" placeholder="Enter Email" id="example-text-input">
+		</div>
+	</div>
+	<div class="form-group row">
+		<label for="example-text-input" class="col-md-3 col-form-label">Password</label>
+		<div class="col-md-8">
+			<input class="form-control" type="password" name="reg_password" placeholder="Enter Password" id="example-text-input">
+		</div>
+	</div>
+	<div class="form-group row">
+		<label for="example-text-input" class="col-md-3 col-form-label">Gender</label>
+		<div class="col-md-8">
+			 <select class="form-control" name="gender">
+			  <option value="volvo">Select Gender</option>
+			  <option value="Male">Male</option>
+			  <option value="Female">Female</option>
+			</select> 
+		</div>
+	</div>
+	<button class="btn btn-primary">Register</button>
+	<input type="hidden" name="action" value="registrar">
+  </form>
+  <br/>
+	Already have an account? <a href="login.php">Click here</a>
+	
+</div>
+</div>
 </body>
 </html>
 
