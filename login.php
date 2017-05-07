4755cbd9e1a368e2726f28ce07400945c26fb0ad
vamsi krishna surapaneni <vks35@afsconnect2.njit.edu>
2017-05-07 17:08:30 -0400

    edit form in login

--- login.php
+++ login.php
@@ -12,17 +12,26 @@
 <div class="container">&nbsp;</div>
 <div class="container">
 <div class="row">
-<html>
-<body>
-  <h1> This is a login page</h1>
-  <form method = "post" action="index.php">
-  <strong>Username:</strong><input type="test" name="reg_uname" value=""/><br><br>
-  <strong>Password:</strong><input type="password" name="reg_password" value=""/><br><br>
-  <input type ="hidden" name="action" value="test_user"><br>
-  <input type="submit" value="Login"/>
+
+ <form method="post" action="index.php" name="form1">
+	<div class="form-group row">
+		<label for="example-text-input" class="col-md-3 col-form-label">username</label>
+		<div class="col-md-8">
+			<input class="form-control" type="text" name="reg_uname" placeholder="Enter Email Address" id="example-text-input">
+		</div>
+	</div>
+	<div class="form-group row">
+		<label for="example-text-input" class="col-md-3 col-form-label">Password</label>
+		<div class="col-md-8">
+			<input class="form-control" type="password" name="reg_password" placeholder="Enter Password" id="example-text-input">
+			<input type="hidden" name="action" value="test_user"/>
+		</div>
+	</div>
+	<button id="ed_submit" name="submit" class="btn btn-primary">Login</button>
   </form>
-  <form action = 'register.php'>
-      <input type = 'submit' value = 'register' />
-  </form>
+  <br/>
+  Not have an account? <a href="register.php">Click here</a> 
+</div>
+</div>
 </body>
 </html>
