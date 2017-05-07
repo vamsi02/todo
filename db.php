a22c1a728084c685ba0c4a2fa570bb6c454c1c56
vamsi krishna surapaneni <vks35@afsconnect2.njit.edu>
2017-05-07 18:12:27 -0400

    create user and validate user

--- db.php
+++ db.php
@@ -1,30 +1,38 @@
 <?php
-   function isUserValid($username,$password){
-     global $db;
-     $query = 'select * from users where username = :name and 
-     passwordHash = :pass';
-     $statement = $db->prepare($query);
-     $statement->bindValue(':name',$username);
-     $statement->bindValue(':pass',$password);
-     $statement->execute();
-     $result= $statement->fetchAll();
-     $statement->closeCursor();
 
-     $count = $statement->rowCount();
-     if($count == 1){
-       setcookie('login' , $username);
-       setcookie('my_id' , $result[0]['id']);
-       setcookie('isLogged' , true);
-       return true;
-     }
-     else{
-       unset($_COOKIE['login']);
-       setcookie('login' , false);
-       setcookie('isLogged' , false);
-       setcookie('my_id' , false);
+function createUser($user_fname,$user_lname,$user_pnumber,$user_email, $reg_password,$gender)
+{
+ global $db;
+ $stmt = $db->query("select * from users where email='".$_POST['reg_username']."'");
+ $rcount = $stmt->rowCount();
+	 if($rcount > 0)
+	{
+		return true;
+	}
+	else
+	{
+		$sqlInst = $db->query("insert into users set fname='".$_POST['user_fname']."',lname='".$_POST['user_lname']."',phone='".$_POST['user_pnumber']."',email='".$_POST['reg_username']."',password='".$_POST['reg_password']."',gender='".$_POST['gender']."'");
+		return false;
+	}
+}
+function isUserValid($user_email, $password)
+{
+  global $db;
+  $stmt = $db->query("select * from users where email='".$user_email."' and password='".$password."'");
+  $count=$stmt->rowCount();
+  $result = $stmt->fetch();
+ 
+
+  if($count == 1){
+
+   $_SESSION["my_id"] =$result['id'];
+  $_SESSION["email"] =$result['email'];
+  
+        return true;
+     }else{
+     
        return false;
      }
-
-   }
+}
 
 ?>
