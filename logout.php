6b9dd3de5e564044ef84fe749d130570cf054fa2
vamsi krishna surapaneni <vks35@afsconnect2.njit.edu>
2017-05-07 20:18:06 -0400

    logout

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+unset($_COOKIE['login']);
+setcookie('login', false);
+setcookie('islogged', false);
+setcookie('my_id', false);
+setcookie('myfname', false);
+setcookie('mylname', false);
+setcookie('mylname', false);
+setcookie('myemail', false);
+header("location:index.php");
+//return false;
+
+exit;
+?>
